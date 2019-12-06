f54cdeb02e2fc5a404d9b19c915df51cb2ba4988
Masle <masle.romain@gmail.com>
2019-12-06 15:49:55 +0100

    Entities structure : 'User' et 'Hobbie'

---
+++ src/main/java/com/aelion/appliActivite/persistances/entities/Hobbie.java
@@ -0,0 +1,48 @@
+package com.aelion.appliActivite.persistances.entities;
+
+public class Hobbie {
+	
+	private Long id;
+	private String label;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	@Override
+	public String toString() {
+		return "Hobbie [id=" + id + ", label=" + label + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hobbie other = (Hobbie) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
---
+++ src/main/java/com/aelion/appliActivite/persistances/entities/User.java
@@ -0,0 +1,95 @@
+package com.aelion.appliActivite.persistances.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class User {
+	
+	private Long id;
+	private String name;
+	private String firstName;
+	private LocalDate birthDate;
+	private String nickname;
+	private List<Hobbie> hobbies;
+	private String description;
+	private String password;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public List<Hobbie> getHobbies() {
+		return hobbies;
+	}
+	public void setHobbies(List<Hobbie> hobbies) {
+		this.hobbies = hobbies;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", firstName=" + firstName + ", birthDate=" + birthDate
+				+ ", nickname=" + nickname + ", hobbies=" + hobbies + ", description=" + description + ", password="
+				+ password + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
