ed010bd962ca6c5308213987b92dc39f1b936c30
ben49610 <ben49.pinon@gmail.com>
2019-12-06 14:43:33 +0100

    first commit

---
+++ src/main/java/com/aelion/appliActivite/AppliActiviteApplication.java
@@ -0,0 +1,13 @@
+package com.aelion.appliActivite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliActiviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliActiviteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aelion/appliActivite/AppliActiviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aelion.appliActivite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliActiviteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
