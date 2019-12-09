7bca6c09f3ef265d7b5b0c2cfd77c775ac7d9e4c
Masle <masle.romain@gmail.com>
2019-12-09 12:26:09 +0100

    merge

--- src/main/java/com/aelion/appliActivite/persistances/entities/User.java
+++ src/main/java/com/aelion/appliActivite/persistances/entities/User.java
@@ -21,6 +21,11 @@ import javax.persistence.Table;
 public class User implements Serializable{
 	
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
