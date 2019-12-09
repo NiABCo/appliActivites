df04c32691a34ef8c301181b2dc562108292f591
Benji49610 <40828741+Benji49610@users.noreply.github.com>
2019-12-09 18:35:19 +0100

    Update User.java

--- src/main/java/com/aelion/appliActivite/persistances/entities/User.java
+++ src/main/java/com/aelion/appliActivite/persistances/entities/User.java
@@ -38,7 +38,7 @@ public class User implements Serializable{
 	@Column(name="nickname", nullable = true)
 	private String nickname;
 	
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
 	@JoinColumn(name = "id_hobbie", nullable = false)
 	private List<Hobby> hobbies;
 
@@ -52,11 +52,11 @@ public class User implements Serializable{
 	@Column(name = "photo", nullable = true)
 	private String photo;
 
-	@OneToMany( cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@OneToMany( cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
 	@JoinColumn(name = "id_sender", nullable = false)
 	private List<Message> sendMsg;
 	
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
 	@JoinColumn(name = "id_receiver", nullable = false)
 	private List<Message> receivedMsg;
 	
