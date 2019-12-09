619fc0021dd9d9e83d94cacd65885724a6925a81
Masle <masle.romain@gmail.com>
2019-12-09 15:27:13 +0100

    Message Entity/DTO/Service/Controller

---
+++ src/main/java/com/aelion/appliActivite/dto/MessagePost.java
@@ -0,0 +1,102 @@
+package com.aelion.appliActivite.dto;
+
+import java.time.LocalDateTime;
+
+public class MessagePost {
+
+	private String title;
+
+	private String body;
+
+	private LocalDateTime sendTime;
+	
+	private Long idSender;
+	
+	/**
+	 * GETTERS SETTERS
+	 */
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public LocalDateTime getSendTime() {
+		return sendTime;
+	}
+
+	public void setSendTime(LocalDateTime sendTime) {
+		this.sendTime = sendTime;
+	}
+
+	public Long getIdSender() {
+		return idSender;
+	}
+
+	public void setIdSender(Long idSender) {
+		this.idSender = idSender;
+	}
+
+	@Override
+	public String toString() {
+		return "MessagePost [title=" + title + ", body=" + body + ", sendTime=" + sendTime + ", idSender=" + idSender
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((body == null) ? 0 : body.hashCode());
+		result = prime * result + ((idSender == null) ? 0 : idSender.hashCode());
+		result = prime * result + ((sendTime == null) ? 0 : sendTime.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MessagePost other = (MessagePost) obj;
+		if (body == null) {
+			if (other.body != null)
+				return false;
+		} else if (!body.equals(other.body))
+			return false;
+		if (idSender == null) {
+			if (other.idSender != null)
+				return false;
+		} else if (!idSender.equals(other.idSender))
+			return false;
+		if (sendTime == null) {
+			if (other.sendTime != null)
+				return false;
+		} else if (!sendTime.equals(other.sendTime))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
