e1d922b183f3a69f4b569e7d3a02b5be7aadc1d9
Benji49610 <40828741+Benji49610@users.noreply.github.com>
2019-12-09 09:37:32 +0100

    Create Message.java

---
+++ src/main/java/com/aelion/appliActivite/persistances/entities/Message.java
@@ -0,0 +1,67 @@
+package com.aelion.appliActivite.persistances.enteties;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.ColumnResult;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Message {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String title;
+	
+	@Column
+	private String body;
+	
+	@Column
+	private LocalDateTime sendTime;
+	
+	@Column
+	private String status;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public LocalDateTime getSendTime() {
+		return sendTime;
+	}
+
+	public void setSendTime(LocalDateTime sendTime) {
+		this.sendTime = sendTime;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [title=" + title + ", body=" + body + ", sendTime=" + sendTime + ", status=" + status + "]";
+	}
+	
+	
+}
