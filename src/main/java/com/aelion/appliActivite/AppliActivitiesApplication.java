e69b4372dda69cabc4c2d05b75aa8bf504e666d3
NiAbCo <nico.abbadie@gmail.com>
2019-12-10 10:14:24 +0100

    added batman

--- src/main/java/com/aelion/appliActivite/AppliActivitiesApplication.java
+++ src/main/java/com/aelion/appliActivite/AppliActivitiesApplication.java
@@ -1,13 +1,41 @@
 package com.aelion.appliActivite;
-
+import java.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.aelion.appliActivite.persistances.entities.User;
+import com.aelion.appliActivite.services.IUserService;
 
 @SpringBootApplication
-public class AppliActivitiesApplication {
+public class AppliActivitiesApplication implements CommandLineRunner{
 
+	@Autowired
+	IUserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppliActivitiesApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		createUser();
+	}
+	
+	public void createUser() {
+		
+		LocalDate birthDate = LocalDate.now();
+		
+		User user = new User();
+		user.setLastname("Wayne");
+		user.setFirstName("Bruce");
+		user.setBirthDate(birthDate);
+		user.setDescription("héros justicier");
+		user.setNickname("Le Chevalier Noir");
+		user.setPassword("taliaAlGhul");
+		userService.save(user);
+	}
+	
 
 }
