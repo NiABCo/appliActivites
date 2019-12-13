4abc2bbe7522454c66510eb5dbffc10c64d6f024
Masle <masle.romain@gmail.com>
2019-12-13 11:30:10 +0100

    UserControl javadoc

--- src/main/java/com/aelion/appliActivite/controllers/UserController.java
+++ src/main/java/com/aelion/appliActivite/controllers/UserController.java
@@ -22,7 +22,12 @@ import com.aelion.appliActivite.dto.UserPost;
 import com.aelion.appliActivite.persistances.entities.User;
 import com.aelion.appliActivite.services.IUserService;
 
-
+/**
+ * Class handling the control of Users
+ * 
+ * @author Aelion
+ *
+ */
 @RestController
 @RequestMapping(path = "/admin/user")
 public class UserController {
@@ -33,22 +38,48 @@ public class UserController {
 	@Autowired
 	ModelMapper mapper;
 	
+	
+	/**
+	 * Return the list of all users
+	 * 
+	 * @return The list of DTO of the users
+	 */
+	
 	@GetMapping("/list")
 	public List<UserLightDTO> getAllUsers(){
 		return userService.findAll().stream().map(user -> mapper.map(user, UserLightDTO.class)).collect(Collectors.toList());
 	}
 	
+	/**
+	 * Return the user with this id "id"
+	 * 
+	 * @throws NotFoundException if user not found
+	 * @param id of the user to return
+	 * @return The DTO of the user with the given id
+	 */
 	@GetMapping("/{id}")
 	public UserFullDTO getUserById(@PathVariable(name = "id") Long id) {
 		return mapper.map(userService.findOne(id), UserFullDTO.class) ;
 	}
 	
+	/**
+	 * Remove a User from the Database
+	 * 
+	 * @throws NotFoundException if user not found
+	 * @param id : id of the user to delete from the DB
+	 * @return true if successful, false if not.
+	 */
 	@DeleteMapping("/{id}")
 	public boolean deleteUserById(@PathVariable(name = "id") Long id) {
 		return  userService.deleteById(id);
 	}
 	
-	
+	/**
+	 * Save a new user to the Database with a Post
+	 * 
+	 * @param userP Request Body of the request. Must be an UserPost
+	 * @return Reponse send to the front
+	 */
 	@PostMapping()
 	public ResponseEntity<String> saveUser(@Valid @RequestBody UserPost userP) {
 		User user = mapper.map(userP, User.class);
